986c7e5e5e2473b147264b702ea279c77f40bc41
Jothilax <135149388+Jothilax@users.noreply.github.com>
2024-09-06 16:04:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
     {/* <Signup/> */}
     <Navigationbar />
     <Routes>
+     <Route path='/' element={<Home />}/>
       <Route path='/home' element={<Home />}/>
       <Route path='/about' element={<About />}/>
       <Route path='/bikelisting' element={<BikeListing />}/>
